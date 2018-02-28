8b3ecaa5fe44f01ed9ab52aacf68ae2ab8f5a3b3
oleksandr.yeropudov <oleksandr.yeropudov@gmail.com>
2018-02-28 20:16:57 +0300

    products models

--- shop/models/product.py
+++ shop/models/product.py
@@ -5,9 +5,37 @@ class Product(models.Model):
     pass
 
 
+class ProductVariant(models.Model):
+    pass
+
+
+class ProductImage(models.Model):
+    pass
+
+
+class VariantImage(models.Model):
+    pass
+
+
+class Stock(models.Model):
+    pass
+
+
+class StockLocation(models.Model):
+    pass
+
+
 class ProductToCatalog(models.Model):
     pass
 
 
 class ProductToProperties(models.Model):
+    pass
+
+
+class ProductAttribute(models.Model):
+    pass
+
+
+class ProductAttributes(models.Model):
     pass
