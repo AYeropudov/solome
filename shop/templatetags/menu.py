a8569d1ce98345af2e5babcb817a7d566dd60ade
alex <oleksandr.yeropudov@gmail.com>
2018-03-10 17:31:17 +0300

    fix templates

--- shop/templatetags/menu.py
+++ shop/templatetags/menu.py
@@ -14,3 +14,16 @@ def menu_block():
         })
 
     return {'tree': menu_tree}
+
+
+@register.inclusion_tag('menu_footer_catalog.html')
+def menu_catalog_footer():
+    roots = Catalog.objects.root_nodes()
+    menu_tree = []
+    for root in roots:
+        menu_tree.append({
+            'title': root.title,
+            'code': root.code,
+        })
+
+    return {'tree': menu_tree}
