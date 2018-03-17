7f9007d3fdec9315b1fe3db67a9aa675a776de48
alex <oleksandr.yeropudov@gmail.com>
2018-03-17 18:46:10 +0300

    убрал Крошки с некоторых страниц

--- shop/models/catalog.py
+++ shop/models/catalog.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 from imagekit.models import ImageSpecField
 from mptt.models import MPTTModel, TreeForeignKey
-from pilkit.processors import ResizeToFit
+from pilkit.processors import ResizeToFit, ResizeToCover
 
 
 def upload_to(instance, filename):
@@ -45,7 +45,7 @@ class CatalogImage(models.Model):
                           format='PNG',
                           options={'quality': 60})
     thumbnail = ImageSpecField(source='link',
-                               processors=[ResizeToFit(width=370, height=370, mat_color=(189, 189, 189, 0))],
+                               processors=[ResizeToCover(width=370, height=370)],
                                format='PNG',
                                options={'quality': 60})
     is_delete = models.BooleanField(default=False, blank=True)
