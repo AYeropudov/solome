2201c0a8e6ebe0d65cae698507a2d896189b850e
Oleksandr Yeropudov <oleksandr.yeropudov@gmail.com>
2018-03-26 17:15:29 +0300

    кастомная админка для товаров

--- cms/views/products/add.py
+++ cms/views/products/add.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from shop.models import Product, Catalog, ProductClass
+from cms.adapters import AdapterProduct
+from cms.adapters.exceptions import ProductException
 
 
 class ProductsAddView(View):
@@ -10,9 +12,22 @@ class ProductsAddView(View):
         catalogs = Catalog.objects.all()
         pclasess = ProductClass.objects.all()
         return render(request=request, template_name='cms/products/add.html',
-                      context={"title": "Товары", "product": product, "catalogs": catalogs, "product_class": pclasess})
+                      context={"title_page": "Товары", "product": product, "catalogs": catalogs, "product_class_list": pclasess})
 
     def post(self, request):
-
-        return render(request=request, template_name='cms/products/add.html',
-                      context={"title": "Товары", "product": None})
+        keys = request.POST.keys()
+        prevalidate = {}
+        for key in keys:
+            if request.POST[key] == '':
+                prevalidate[key]='Это обязательное поле'
+        if len(prevalidate.keys()) > 0:
+            return JsonResponse(data={"errors": prevalidate}, status=400)
+        try:
+            result = AdapterProduct.create_new_product(form_data=request.POST, files=request.FILES)
+        except ProductException as e:
+            catalogs = Catalog.objects.all()
+            pclasess = ProductClass.objects.all()
+            _context = {}
+            _context['errors']= e.errors
+            return JsonResponse(data=_context, status=400)
+        return JsonResponse({'ok': ""})
