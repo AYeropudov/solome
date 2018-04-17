f0c298879bcd775153f8c1b23d49b489ecb98273
Oleksandr Yeropudov <oleksandr.yeropudov@gmail.com>
2018-04-17 17:03:16 +0300

    Рандомная плитка товаров на главной

--- shop/views/indexView.py
+++ shop/views/indexView.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from shop.models import Catalog
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request=request, template_name='index.html', context={"is_breadcrumbs": False, "landing": True})
+        _context = {"products": [], "is_breadcrumbs": False, "landing": True, 'nodes': Catalog.objects.root_nodes()}
+        for node in _context['nodes']:
+            products = list(node.producttocatalog_set.all().order_by('?')[:4])
+            _context['products'] = _context['products'] + products
+
+        return render(request=request, template_name='index.html', context=_context)
         # return HttpResponse("Hello, world. You're at the index.")
