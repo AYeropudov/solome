c0eabdc9d0f1fcdd44da5db73cd7727814e95151
alex <oleksandr.yeropudov@gmail.com>
2018-04-20 20:28:56 +0300

    fix product model

--- shop/models/product.py
+++ shop/models/product.py
@@ -42,7 +42,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     is_delete = models.BooleanField(default=False, blank=True)
-    brand = models.ForeignKey(ProductBrand, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey(ProductBrand, blank=True, on_delete=models.DO_NOTHING)
     code = models.CharField(max_length=255)
 
     def __str__(self):
